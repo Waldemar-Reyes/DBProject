36a8376c46178d6ca615aaa167c418a0afb6ec2d
Waldemar Reyes <waldemar.reyes@upr.edu>
2020-03-29 15:48:22 -0400

    Implemented full schema (genericly)

--- schema.sql
+++ schema.sql
@@ -1,15 +1,37 @@
 -- This file contains the definitions of the tables used in the application.
 --
--- Part table
-create table parts(pid serial primary key, pname varchar(20), pmaterial varchar(10), pcolor varchar(10), pprice float);
+-- Company table
+create table company(compid serial primary key, compname varchar(20));
+
+-- Consumer table
+create table consumer(consid serial primary key, consusername varchar(20));
+
+-- Order table
+create table orders(odid serial primary key, odnumber int);
+
+-- Pay Method table
+create table pay_method(pmid serial primary key, pmname varchar(20));
+
+-- Reservation table
+create table reservation(resid serial primary key, restime varchar(20));
+
+-- Resource table
+create table resource(rid serial primary key, rname varchar(20), rprice int, rlocation varchar(20), ramount int);
 
 -- Supplier table
-create table supplier(sid serial primary key, sname varchar(10), scity varchar(10), sphone varchar(10));
+create table supplier(sid serial primary key, susername varchar(20), scompany varchar(20));
 
--- Supplies table
-create table supplies(pid integer references Parts(pid), sid integer references
-Supplier(sid), qty integer, primary key(pid, sid));
+-- System Admin table
+create table sys_adm(said serial primary key, sausername varchar(20));
 
--- PartSales table
-create table partsales(psaleid serial primary key, pid integer references Parts(pid),
-sid integer references Supplier(sid), sqty integer, sprice float, sdate Date);
+-- User table
+create table user(uid serial primary key, ufirstname varchar(20), ulastname varchar(20));
+
+
+---- Supplies table
+--create table supplies(pid integer references Parts(pid), sid integer references
+--Supplier(sid), qty integer, primary key(pid, sid));
+--
+---- PartSales table
+--create table partsales(psaleid serial primary key, pid integer references Parts(pid),
+--sid integer references Supplier(sid), sqty integer, sprice float, sdate Date);
