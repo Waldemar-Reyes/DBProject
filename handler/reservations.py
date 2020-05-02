d9b4d5a257afb81f57998636704d9fb2986bf7f7
Juliodl9607 <64420798+Juliodl9607@users.noreply.github.com>
2020-05-02 13:00:09 +0200

    Update reservations.py

--- handler/reservations.py
+++ handler/reservations.py
@@ -1,2 +1,49 @@
-# -*- coding: utf-8 -*-
+from flask import jsonify
+from dao.reservation import ReservationDAO
 
+class ReservationHandler:
+    def build_reservation_dict(self, row):
+        result = {}
+        result['resid'] = row[0]
+        result['restime'] = row[1]
+        return result
+      
+     def build_reservation_attributes(self, resid, restime):
+        result = {}
+        result['resid'] = resid
+        result['restime'] = restime
+        return result
+
+    def insertReservationJson(self, json):
+        reservation = json['restime']
+        if reservation:
+            dao = ReservationDAO()
+            redid = dao.insert(restime)
+            result = self.build_reservation_attributes(resid, restime)
+            return jsonify(Reservation=result), 201
+        else:
+            return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def updateReservation(self, resid, form):
+        dao = ReservationDAO()
+        if not dao.getreservationById(resid):
+            return jsonify(Error="Reservation not found."), 404
+        else:
+            if len(form) != 1:
+                return jsonify(Error="Malformed update request"), 400
+            else:
+                restime = form['restime']
+                if restime:
+                    dao.update(resid, restime)
+                    result = self.build_reservation_attributes(resid, restime)
+                    return jsonify(Reservation=result), 200
+                else:
+                    return jsonify(Error="Unexpected attributes in update request"), 400
+
+    def deletereservation(self, resid):
+        dao = ReservationDAO()
+        if not dao.getReservationById(resid):
+            return jsonify(Error="Reservation not found."), 404
+        else:
+            dao.delete(resid)
+            return jsonify(DeleteStatus="OK"), 200
