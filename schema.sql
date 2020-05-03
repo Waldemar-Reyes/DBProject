e439b8646468e81f4ace5617ea9539e90b25782b
bt3fan1 <john.hernandez3@upr.edu>
2020-05-03 19:27:05 -0400

    Schema tables added

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ create table pay_method(pmid serial primary key, pmname varchar(20));
 create table reservation(resid serial primary key, restime varchar(20));
 
 -- Resource table
-create table resources(rid serial primary key, rname varchar(20), rprice int, rlocation varchar(20), ramount int);
+create table resources(rid serial primary key, rname varchar(20), rtype varchar(20), rprice int, rlocation varchar(20), ramount int);
 
 -- Supplier table
 create table supplier(sid serial primary key, susername varchar(20), scompany varchar(20));
@@ -26,3 +26,33 @@ create table sys_adm(said serial primary key, sausername varchar(20));
 
 -- User table
 create table users(uid serial primary key, ufirstname varchar(20), ulastname varchar(20));
+
+-- Request table
+create table request(reqid serial primary key, consid integer references consumer(consid), reqname varchar(20), reqtype varchar(20), reqprice int, reqlocation varchar(20), reqamount int);
+
+-- Consumer to Pay Method Table
+create table owns(pmid integer references pay_method(pmid), consid integer references consumer(consid), primary key (pmid, consid));
+
+-- Consumer to Orders Table
+create table makes(odid integer references orders(odid), consid integer references consumer(consid), primary key (odid, consid));
+
+-- Consumer to Request Table
+create table requests(reqid integer references request(reqid), consid integer references consumer(consid), primary key (reqid, consid));
+
+-- Order to Resources Table
+create table belongs(rid integer references resources(rid), odid integer references orders(odid), primary key (rid, odid), quantity int);
+
+-- Pay Method to Orders Table
+create table pays(odid integer references orders(odid), pmid integer references pay_method(pmid), primary key (odid, pmid));
+
+-- Supplier to Company Table
+create table works(compid integer references company(compid), sid integer references supplier(sid), primary key (compid, sid));
+
+-- Supplier to Resources Table
+create table supplies(sid integer references supplier(sid), rid integer references resources(rid), primary key (sid, rid));
+
+-- System Admin to User Table
+create table manages(uid integer references users(uid), said integer references system_admin(said), primary key (uid, said));
+
+-- Request to Resources Table
+create table asks(reqid integer references request(reqid), rid integer references resources(rid), primary key (reqid, rid));
