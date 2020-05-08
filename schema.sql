882f8b5a0278b6eab639770b5b3f5d577bcc5158
Waldemar Reyes <waldemar.reyes@upr.edu>
2020-05-08 01:22:50 -0400

    Updated Values

--- schema.sql
+++ schema.sql
@@ -1,31 +1,31 @@
 -- This file contains the definitions of the tables used in the application.
 --
 -- Company table
-create table company(compid serial primary key not null, compname varchar(20) not null);
+create table company(compid serial primary key not null, compname varchar(127) not null);
 
 -- Users table
-create table users(uid serial primary key not null, ufirstname varchar(20) not null, ulastname varchar(20) not null);
+create table users(uid serial primary key not null, ufirstname varchar(127) not null, ulastname varchar(127) not null);
 
 -- Consumer table
-create table consumer(consid serial primary key not null, uid integer references users(uid), consusername varchar(20) not null);
+create table consumer(consid serial primary key not null, uid integer references users(uid), consusername varchar(127) not null);
 
 -- Pay Method table
-create table pay_method(pmid serial primary key not null, pmname varchar(20) not null);
+create table pay_method(pmid serial primary key not null, pmname varchar(127) not null);
 
 -- Request table
-create table reservation(resid serial primary key not null, resname varchar(20) not null, restype varchar(20) not null, resprice double precision not null, reslocation varchar(255), resstock int not null, restime varchar(255) not null);
+create table reservation(resid serial primary key not null, resname varchar(127) not null, restype varchar(127) not null, resprice double precision not null, reslocation varchar(255), resstock int not null, restime varchar(127) not null);
 
 -- Resources table
-create table resources(rid serial primary key not null, rname varchar(20) not null, rtype varchar(20) not null, rprice double precision not null, rlocation varchar(255), rstock int not null);
+create table resources(rid serial primary key not null, rname varchar(127) not null, rtype varchar(127) not null, rprice double precision not null, rlocation varchar(255), rstock int not null);
 
 -- Supplier table
-create table supplier(sid serial primary key not null, uid integer references users(uid), susername varchar(20) not null, scompany varchar(20) not null);
+create table supplier(sid serial primary key not null, uid integer references users(uid), susername varchar(127) not null, scompany varchar(127) not null);
 
 -- System Admin table
-create table sys_adm(said serial primary key not null, uid integer references users(uid), sausername varchar(20) not null);
+create table sys_adm(said serial primary key not null, uid integer references users(uid), sausername varchar(127) not null);
 
 -- Order table
-create table orders(odid serial primary key not null, resid integer references reservation(resid), odnumber int not null, odtime varchar(255) not null);
+create table orders(odid serial primary key not null, resid integer references reservation(resid), odnumber int not null, odtime varchar(127) not null);
 
 -- Consumer to Pay Method Table
 create table owns(pmid integer references pay_method(pmid), consid integer references consumer(consid), primary key (pmid, consid) not null );
