7d123d799e1b226fd3079e1e8f865312b8eeff71
Waldemar Reyes <waldemar.reyes@upr.edu>
2020-05-08 01:27:00 -0400

    Removed invalid keys

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+
 -- This file contains the definitions of the tables used in the application.
 --
 -- Company table
@@ -28,28 +29,28 @@ create table sys_adm(said serial primary key not null, uid integer references us
 create table orders(odid serial primary key not null, resid integer references reservation(resid), odnumber int not null, odtime varchar(127) not null);
 
 -- Consumer to Pay Method Table
-create table owns(pmid integer references pay_method(pmid), consid integer references consumer(consid), primary key (pmid, consid) not null );
+create table owns(pmid integer references pay_method(pmid), consid integer references consumer(consid), primary key (pmid, consid));
 
 -- Consumer to Orders Table
-create table makes(odid integer references orders(odid), consid integer references consumer(consid), primary key (odid, consid) not null);
+create table makes(odid integer references orders(odid), consid integer references consumer(consid), primary key (odid, consid));
 
 -- Order to Resources Table
-create table belongs(rid integer references resources(rid), odid integer references orders(odid), primary key (rid, odid) not null, odquantity int not null);
+create table belongs(rid integer references resources(rid), odid integer references orders(odid), primary key (rid, odid), odquantity int not null);
 
 -- Pay Method to Orders Table
-create table pays(odid integer references orders(odid), pmid integer references pay_method(pmid), primary key (odid, pmid) not null);
+create table pays(odid integer references orders(odid), pmid integer references pay_method(pmid), primary key (odid, pmid));
 
 -- Supplier to Company Table
-create table works(compid integer references company(compid), sid integer references supplier(sid), primary key (compid, sid) not null);
+create table works(compid integer references company(compid), sid integer references supplier(sid), primary key (compid, sid));
 
 -- Supplier to Resources Table
-create table supplies(sid integer references supplier(sid), rid integer references resources(rid), primary key (sid, rid) not null);
+create table supplies(sid integer references supplier(sid), rid integer references resources(rid), primary key (sid, rid));
 
 -- System Admin to Users Table
-create table manages(uid integer references users(uid), said integer references sys_adm(said), primary key (uid, said) not null);
+create table manages(uid integer references users(uid), said integer references sys_adm(said), primary key (uid, said));
 
 -- Reservations to Resources Table
-create table asks(resid integer references reservation(resid), rid integer references resources(rid), primary key (resid, rid) not null, resquantity int not null);
+create table asks(resid integer references reservation(resid), rid integer references resources(rid), primary key (resid, rid), resquantity int not null);
 
 -- Consumer to Reservations Table
-create table requests(resid integer references reservation(resid), consid integer references consumer(consid), primary key (resid, consid) not null);
+create table requests(resid integer references reservation(resid), consid integer references consumer(consid), primary key (resid, consid));
